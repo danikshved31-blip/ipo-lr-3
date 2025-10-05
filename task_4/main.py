91e4ba214536498ec1a27cf456c725f0ee2afb9a
danchik <danikshved31@gmail.com>
2025-10-06 00:06:24 +0300

    Упс не тот вариант

--- task_4/main.py
+++ task_4/main.py
@@ -1,7 +1,7 @@
-num = list(map(float, input("Введите 3 чила через пробел (чило месяц)").split()))
-if num[0] < num[1] and num[0] < num[2]:
-    print(f"Наименьшее число {num[0]}")
-elif num[1] < num[0] and num[1] < num[2]:
-    print(f"Наименьшее число {num[1]}")
-    elif num[2] < num[1] and num[2] < num[0]:
-print(f"Наименьшее число {num[2]}")
+num = input('Введите пароль: ')
+password = "kisikisi" 
+if num == password:
+    print("Доступ разрешен")
+else:
+    print('Доступ запрещен')
+
